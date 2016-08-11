6e1be0f8be11e4257132deff5d3ce7f7ef9b1ca8
Philipp von Hammerstein <p.vonhammerstein@whu.edu>
2016-08-11 15:53:06 +0200

    Basic version without much design

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,25 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail, only: [ :new, :create ]
 
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = @cocktail.doses.build(dose_params)
+    @dose.save!
+    redirect_to cocktail_path(@cocktail)
+  end
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
 
 end
