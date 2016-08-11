82f11f8f42bf5de532e379eb3a207e3f8fb32bb9
Philipp von Hammerstein <p.vonhammerstein@whu.edu>
2016-08-11 15:07:54 +0200

    Added controller for cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,21 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params.require(:cocktail).permit(:name))
+    @cocktail.save!
+    redirect_to cocktail_path(@cocktail)
+  end
+
 end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,6 @@
+class DosesController < ApplicationController
+
+
+
+end
+
